9d39f6e339b0ec1a63d8d7f53fa67b24b2337891
GalinaSolopiy <sharicbo@gmail.com>
2018-02-26 18:19:43 +0300

    task ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var x1 = number;
+    var x2;
+    var count = 0;
+    do {
+        x2 = Math.floor(x1/5);
+        count += x2;
+        x1 = x2;
+    } while (x1>1);
+    return count;
 }
